8f70b1dbbf77b508a56b8bd004b955952e0460e5
serhack <27734319+serhack@users.noreply.github.com>
2017-12-04 20:58:27 +0100

    Create monero.php

---
+++ catalog/model/extension/payment/monero.php
@@ -0,0 +1,17 @@
+<?php
+class ModelExtensionPaymentMonero extends Model {
+  	public function getMethod($address, $total) {
+		
+		
+		
+      		$method_data = array( 
+        		'code'         	=> 'monero',
+        		'title'      	=> 'Monero Payment Gateway',
+      		'sort_order' => $this->config->get('custom_sort_order'),
+      		'terms' => 'by serhack'
+      		);
+    	
+   
+    	return $method_data;
+  	}
+}
