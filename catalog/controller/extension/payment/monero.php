7e6553a2f53a21eec1b41a87d8bf5f46661ef9c0
serhack <27734319+serhack@users.noreply.github.com>
2017-12-29 21:28:23 +0100

    Update monero.php

--- catalog/controller/extension/payment/monero.php
+++ catalog/controller/extension/payment/monero.php
@@ -65,9 +65,9 @@ class ControllerExtensionPaymentMonero extends Controller {
 	public function make_integrated_address(){
 		    $this->load->library('jsonrpclibrary');
 		    $this->load->library('monero');
-		/* TODO REMOVE ME */
-		    $host = "";
-		    $port = "";
+	
+		    $host = $this->config->get('monero_wallet_rpc_host');
+		    $port = $this->config->get('monero_wallet_rpc_port');
 		$monero = new Monero_Payments($host, $port);
 		$integrated_address = $monero->make_integrated_address();
 		return $integrated_address; 
